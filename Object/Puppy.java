0898eac6ca7a00e83cc9e7c20e21d2eb0db12a1c
francis kamande <franciskamande2001@gmail.com>
2022-03-13 02:16:38 +0300

    I learnt about objects

---
+++ Object/Puppy.java
@@ -0,0 +1,9 @@
+public class Puppy{
+	public  Puppy(String name){
+		System.out.println("Passed Name is: "+name);
+	}
+
+	public static void main(String[] args){
+		Puppy myPuppy = new Puppy("Tommy");
+	}
+}
